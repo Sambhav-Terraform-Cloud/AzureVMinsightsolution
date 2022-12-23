271eaa857653f673f5a1e600e24eabb12bd3cf19
Kumar Sambhav Pandey <sambhavpandey@gmail.com>
2022-12-23 12:08:35 +1100

    Update log analytics workspace.tf

--- log analytics workspace.tf
+++ log analytics workspace.tf
@@ -3,12 +3,12 @@
 resource "azurerm_virtual_machine_extension" "azure-monitor-agent" {
 
   for_each = {
-    "AzureMonitorWindowsAgent" = {virtual_machine_id = "azurerm_windows_virtual_machine.myWindowsVm1.id", version = "1.10.0.0"}
-    "AzureMonitorLinuxAgent" = {virtual_machine_id = "azurerm_linux_virtual_machine.myLinuxVm1.id", version = "1.10.0.0"}
+    "AzureMonitorWindowsAgent" = {id = "azurerm_windows_virtual_machine.myWindowsVm1.id", version = "1.10.0.0"}
+    "AzureMonitorLinuxAgent" = {id = "azurerm_linux_virtual_machine.myLinuxVm1.id", version = "1.10.0.0"}
   }
   
   name                  = each.key
-  virtual_machine_id    = each.value.virtual_machine_id
+  virtual_machine_id    = each.value.id
   publisher             = "Microsoft.Azure.Monitor"
   type                  = each.key
   type_handler_version  =  each.value.version
@@ -23,12 +23,12 @@ resource "azurerm_virtual_machine_extension" "azure-dependency-agent" {
   
   #type as key
   for_each = {
-    "DependencyAgentWindows" = {virtual_machine_id = "azurerm_windows_virtual_machine.myWindowsVm1.id", version = "9.10"}
-    "DependencyAgentLinux" = {virtual_machine_id = "azurerm_linux_virtual_machine.myLinuxVm1.id", version = "9.5"}
+    "DependencyAgentWindows" = {id = "azurerm_windows_virtual_machine.myWindowsVm1.id", version = "9.10"}
+    "DependencyAgentLinux" = {id = "azurerm_linux_virtual_machine.myLinuxVm1.id", version = "9.5"}
   }
   
   name                  = "DAExtension"
-  virtual_machine_id    = each.value.virtual_machine_id
+  virtual_machine_id    = each.value.id
   publisher             = "Microsoft.Azure.Monitoring.DependencyAgent"
   type                  = each.key
   type_handler_version  =  "1.5"
