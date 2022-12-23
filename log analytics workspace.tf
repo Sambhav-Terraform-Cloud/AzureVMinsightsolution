5707701a725945d66be82d6ebe3a7c359f992808
Kumar Sambhav Pandey <sambhavpandey@gmail.com>
2022-12-23 12:35:05 +1100

    Create log analytics workspace.tf

---
+++ log analytics workspace.tf
@@ -0,0 +1,23 @@
+# Log Analytics Workspace
+resource "azurerm_log_analytics_workspace" "law" {
+  name                      = "vmloganalytics"
+  resource_group_name             = data.azurerm_resource_group.rg.name
+  location                        = data.azurerm_resource_group.rg.location
+  sku                       = "PerGB2018"
+  retention_in_days         = 365
+  internet_ingestion_enabled= true
+  internet_query_enabled    = false
+}
+
+# Log analytics solution
+resource "azurerm_log_analytics_solution" "vminsights" {
+  solution_name         = "vminsights"
+  resource_group_name             = data.azurerm_resource_group.rg.name
+  location                        = data.azurerm_resource_group.rg.location
+  workspace_resource_id = azurerm_log_analytics_workspace.law.id
+  workspace_name        = azurerm_log_analytics_workspace.law.name
+  plan {
+    publisher = "Microsoft"
+    product   = "VMInsights"
+  }
+}
