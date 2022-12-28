b13db0865dbad72406aacecc7445e6a5011b9e37
Kumar Sambhav Pandey <sambhavpandey@gmail.com>
2022-12-28 12:49:50 +1100

    Update extensions.tf

--- extensions.tf
+++ extensions.tf
@@ -36,12 +36,12 @@ resource "azurerm_virtual_machine_extension" "azure-dependency-agent" {
   
   #type as key
   for_each = {
-    "DependencyAgentWindows" = {id = "${azurerm_windows_virtual_machine.myWindowsVm1.id}", version = "9.10"}
-    "DependencyAgentLinux" = {id = "${azurerm_linux_virtual_machine.myLinuxVm1.id}", version = "9.5"}
+    "DependencyAgentWindows" = {machine_id = "${azurerm_windows_virtual_machine.myWindowsVm1.id}", version = "9.10"}
+    "DependencyAgentLinux" = {machine_id = "${azurerm_linux_virtual_machine.myLinuxVm1.id}", version = "9.5"}
   }
   
   name                  = "DAExtension"
-  virtual_machine_id    = each.value.id
+  virtual_machine_id    = each.value.machine_id
   publisher             = "Microsoft.Azure.Monitoring.DependencyAgent"
   type                  = each.key
   type_handler_version  =  each.value.version
