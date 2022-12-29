61a8a1903a7d91bf34415e02917ccf2c0b686189
Kumar Sambhav Pandey <sambhavpandey@gmail.com>
2022-12-30 10:13:51 +1100

    Update extensions.tf

--- extensions.tf
+++ extensions.tf
@@ -3,17 +3,34 @@
 resource "azurerm_virtual_machine_extension" "azure-monitor-agent" {
 
   for_each = {
-    "AzureMonitorWindowsAgent" = {id = "${azurerm_windows_virtual_machine.myWindowsVm1.id}", version = "1.10"}
-    "AzureMonitorLinuxAgent" = {id = "${azurerm_linux_virtual_machine.myLinuxVm1.id}", version = "1.24"}
+    "AzureMonitorWindowsAgent" = {machine_id = "${azurerm_windows_virtual_machine.myWindowsVm1.id}", version = "1.10"}
+    "AzureMonitorLinuxAgent" = {machine_id = "${azurerm_linux_virtual_machine.myLinuxVm1.id}", version = "1.24"}
   }
   
   name                  = each.key
-  virtual_machine_id    = each.value.id
+  virtual_machine_id    = each.value.machine_id
   publisher             = "Microsoft.Azure.Monitor"
   type                  = each.key
   type_handler_version  =  each.value.version
   automatic_upgrade_enabled  = true
   auto_upgrade_minor_version = true
+  
+  settings = jsonencode({
+    workspaceId               = azurerm_log_analytics_workspace.law.id
+    azureResourceId           = each.value.machine_id
+    stopOnMultipleConnections = false
+
+    authentication = {
+      managedIdentity = {
+        identifier-name  = "mi_res_id"
+        identifier-value = azurerm_user_assigned_identity.myUserassignedIdentiy.id
+      }
+    }
+
+  })
+  protected_settings = jsonencode({
+    "workspaceKey" = azurerm_log_analytics_workspace.law.primary_shared_key
+  })
   
 /*
     settings = <<SETTINGS
