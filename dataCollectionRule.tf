c2455d0c4e63a331491327b0453be6e6575f9ede
Kumar Sambhav Pandey <sambhavpandey@gmail.com>
2022-12-30 10:09:06 +1100

    Update dataCollectionRule.tf

--- dataCollectionRule.tf
+++ dataCollectionRule.tf
@@ -1,28 +1,28 @@
 # Data Collection Rules
-resource "azurerm_monitor_data_collection_rule" "rule" {
-  name                = "my-dcr"
-  resource_group_name             = data.azurerm_resource_group.rg.name
-  location                        = data.azurerm_resource_group.rg.location
+
+# Data colletion rule - Windows
+resource "azurerm_monitor_data_collection_rule" "rule-windows" {
+  name                = "my-dcr-windows"
 
   destinations {
     log_analytics {
       workspace_resource_id = azurerm_log_analytics_workspace.law.id
-      name                  = "destination-log"
+      name                  = "destination-log-windows"
     }
 
     azure_monitor_metrics {
-      name = "destination-metrics"
+      name = "destination-metrics-windows"
     }
   }
 
   data_flow {
     streams      = ["Microsoft-InsightsMetrics"]
-    destinations = ["destination-metrics"]
+    destinations = ["destination-metrics-windows"]
   }
 
   data_flow {
     streams      = ["Microsoft-InsightsMetrics", "Microsoft-Syslog", "Microsoft-Perf", "Microsoft-WindowsEvent"]
-    destinations = ["destination-log"]
+    destinations = ["destination-log-windows"]
   }
 
   data_sources {
@@ -30,7 +30,7 @@ resource "azurerm_monitor_data_collection_rule" "rule" {
       streams                       = ["Microsoft-Perf", "Microsoft-InsightsMetrics"]
       sampling_frequency_in_seconds = 60
       counter_specifiers            = ["\\VmInsights\\DetailedMetrics"]
-      name                          = "VMInsightsPerfCounters"
+      name                          = "Win-VMInsightsPerfCounters"
     }
 
   }
@@ -40,14 +40,54 @@ resource "azurerm_monitor_data_collection_rule" "rule" {
 }
 
 
+# Data colletion rule - Linux
+
+resource "azurerm_monitor_data_collection_rule" "rule-linux" {
+  name                = "my-dcr-linux"
+
+  destinations {
+    log_analytics {
+      workspace_resource_id = azurerm_log_analytics_workspace.law.id
+      name                  = "destination-log-linux"
+    }
+
+    azure_monitor_metrics {
+      name = "destination-metrics-linux"
+    }
+  }
+
+  data_flow {
+    streams      = ["Microsoft-InsightsMetrics"]
+    destinations = ["destination-metrics-linux"]
+  }
+
+  data_flow {
+    streams      = ["Microsoft-InsightsMetrics", "Microsoft-Syslog", "Microsoft-Perf"]
+    destinations = ["destination-log-linux"]
+  }
+
+  data_sources {
+    performance_counter {
+      streams                       = ["Microsoft-Perf", "Microsoft-InsightsMetrics"]
+      sampling_frequency_in_seconds = 60
+      counter_specifiers            = ["\\VmInsights\\DetailedMetrics"]
+      name                          = "Win-VMInsightsPerfCounters"
+    }
+
+  }
+  depends_on = [
+    azurerm_log_analytics_solution.vminsights
+  ]
+}
+
 resource "azurerm_monitor_data_collection_rule_association" "dcra" {
   
   for_each = {
-        "windowsVM-dcra-" = {machine_id = "${azurerm_windows_virtual_machine.myWindowsVm1.id}", desc = "Windows VM data collection rule association"}
-        "linuxVM-dcra" = {machine_id = "${azurerm_linux_virtual_machine.myLinuxVm1.id}", desc = "Linux VM data collection rule association"}
+        "windowsVM-dcra-" = {machine_id = "${azurerm_windows_virtual_machine.myWindowsVm1.id}", desc = "Windows VM data collection rule association", dcra_id = "${azurerm_monitor_data_collection_rule.rule-windows.id}"}
+        "linuxVM-dcra" = {machine_id = "${azurerm_linux_virtual_machine.myLinuxVm1.id}", desc = "Linux VM data collection rule association", dcra_id = "${azurerm_monitor_data_collection_rule.rule-linux.id}"}
   }
   name                    = each.key
   target_resource_id      = each.value.machine_id
-  data_collection_rule_id = azurerm_monitor_data_collection_rule.rule.id
+  data_collection_rule_id = each.value.dcra_id
   description             = each.value.desc
 }
