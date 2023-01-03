ebabac906369970ac773798ccfc82670b054eed7
Kumar Sambhav Pandey <sambhavpandey@gmail.com>
2023-01-03 13:04:11 +0530

    Create data.tf

---
+++ data.tf
@@ -0,0 +1,15 @@
+data "azurerm_resource_group" "rg" {
+  name = "myrg"
+}
+
+data "azurerm_log_analytics_workspace" "myloganalyticsws {
+  name = "vmloganalytics"
+}
+
+data "azurerm_windows_virtual_machine" "windowsVM" {
+  name = "mywindowsvm1"
+}
+
+data "azurerm_linux_virtual_machine" "linuxVM" {
+  name = "mylinuxvm1"
+}
