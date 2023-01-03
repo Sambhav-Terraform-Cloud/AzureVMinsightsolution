e702bf9ba662e1c84bcd17614a7dad97650f04c7
Kumar Sambhav Pandey <sambhavpandey@gmail.com>
2023-01-03 15:53:13 +1100

    Update and rename log analytics workspace.tf to log analytics solution.tf

--- log analytics solution.tf
+++ log analytics solution.tf
@@ -1,12 +1,3 @@
-# Log Analytics Workspace
-resource "azurerm_log_analytics_workspace" "law" {
-  name                      = "vmloganalytics"
-  resource_group_name             = data.azurerm_resource_group.rg.name
-  location                        = data.azurerm_resource_group.rg.location
-  sku                       = "PerGB2018"
-  internet_query_enabled    = false
-}
-
 # Log analytics solution
 resource "azurerm_log_analytics_solution" "vminsights" {
   solution_name         = "vminsights"
