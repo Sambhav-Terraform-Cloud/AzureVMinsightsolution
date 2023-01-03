5e640e7d51d5d7014fad5559a9ee7ba9d5cb4662
Kumar Sambhav Pandey <sambhavpandey@gmail.com>
2023-01-04 10:17:28 +1100

    Update data.tf

--- data.tf
+++ data.tf
@@ -4,12 +4,15 @@ data "azurerm_resource_group" "rg" {
 
 data "azurerm_log_analytics_workspace" "law" {
   name = "vmloganalytics"
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
-data "azurerm_windows_virtual_machine" "windowsVM" {
+data "azurerm_virtual_machine" "windowsVM" {
   name = "mywindowsvm1"
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
-data "azurerm_linux_virtual_machine" "linuxVM" {
+data "azurerm_virtual_machine" "linuxVM" {
   name = "mylinuxvm1"
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
