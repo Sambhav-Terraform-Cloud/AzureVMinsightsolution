8666181e9a35fa1ae9a636bfd0bbc51bc59be6ca
Kumar Sambhav Pandey <sambhavpandey@gmail.com>
2023-01-03 13:47:31 +1100

    Delete user_assigned_managed_identity.tf

--- user_assigned_managed_identity.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_user_assigned_identity" "myUserassignedIdentity" {
-  name                = "u-id"
-  resource_group_name             = data.azurerm_resource_group.rg.name
-  location                        = data.azurerm_resource_group.rg.location
-}
