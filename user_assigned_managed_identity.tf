2b7b3f72d02c8e85c79506afb2a7a320d02f737f
Kumar Sambhav Pandey <sambhavpandey@gmail.com>
2023-01-03 11:07:13 +1100

    Update user_assigned_managed_identity.tf

--- user_assigned_managed_identity.tf
+++ user_assigned_managed_identity.tf
@@ -1,4 +1,4 @@
-resource "azurerm_user_assigned_identity" "myUserassignedIdentiy" {
+resource "azurerm_user_assigned_identity" "myUserassignedIdentity" {
   name                = "ua-id"
   resource_group_name             = data.azurerm_resource_group.rg.name
   location                        = data.azurerm_resource_group.rg.location
