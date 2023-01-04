29ca9baa18b558e1895c50f89466234c80b7eca7
Kumar Sambhav Pandey <sambhavpandey@gmail.com>
2023-01-04 11:17:25 +1100

    Update log analytics solution.tf

--- log analytics solution.tf
+++ log analytics solution.tf
@@ -1,10 +1,9 @@
-# Log analytics solution
 resource "azurerm_log_analytics_solution" "vminsights" {
-  solution_name                   = "vminsights"
-  resource_group_name             = data.azurerm_resource_group.rg.name
-  location                        = data.azurerm_resource_group.rg.location
-  workspace_resource_id           = data.azurerm_log_analytics_workspace.law.id
-  workspace_name                  = data.azurerm_log_analytics_workspace.law.name
+  solution_name         = "VMInsights"
+  resource_group_name   = data.azurerm_resource_group.rg.name
+  location              = data.azurerm_resource_group.rg.location
+  workspace_resource_id = data.azurerm_log_analytics_workspace.law.id
+  workspace_name        = data.azurerm_log_analytics_workspace.law.name
   plan {
     publisher = "Microsoft"
     product   = "OMSGallery/VMInsights"
