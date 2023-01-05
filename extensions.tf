bf53194050a21e1325d4fdf730f476498e455265
Kumar Sambhav Pandey <sambhavpandey@gmail.com>
2023-01-05 11:13:35 +1100

    Update extensions.tf

--- extensions.tf
+++ extensions.tf
@@ -34,6 +34,8 @@ resource "azurerm_virtual_machine_extension" "azure-monitor-agent" {
 }
 
 */
+  
+/*
 # OMS Agent for Linux
 resource "azurerm_virtual_machine_extension" "OMS" {
   depends_on = [  azurerm_virtual_machine_extension.azureda  ]
@@ -83,6 +85,8 @@ resource "azurerm_virtual_machine_extension" "msmonitor-agent-winodws" {
   PROTECTED_SETTINGS
 }
 
+*/
+
 
 
 # Dependency agent extension
@@ -100,4 +104,18 @@ resource "azurerm_virtual_machine_extension" "azureda" {
   type_handler_version  =  each.value.version
   automatic_upgrade_enabled  = true
   auto_upgrade_minor_version = true
+  
+  settings = <<SETTINGS
+    {
+        "workspaceId": "${data.azurerm_log_analytics_workspace.law.workspace_id}",
+        "azureResourceId": each.value.machine_id,
+        "stopOnMultipleConnections": "false"
+    }
+  SETTINGS
+  protected_settings = <<PROTECTED_SETTINGS
+    {
+      "workspaceKey": "${data.azurerm_log_analytics_workspace.law.primary_shared_key}"
+    }
+  PROTECTED_SETTINGS
+}
 }
