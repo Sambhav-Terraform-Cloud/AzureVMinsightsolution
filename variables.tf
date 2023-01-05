779e8d7edcd271846d276dd9c6f187e318da9e05
Kumar Sambhav Pandey <sambhavpandey@gmail.com>
2023-01-05 13:03:51 +1100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,37 @@
+variable "location" {
+  description = "region"
+  type        = string
+}
+
+variable "scope" {
+  type        = string
+}
+
+variable "cpuThresoldPercent" {
+  type        = string
+}
+
+variable "severity" {
+  type        = string
+}
+
+variable "frequency" {
+  type        = string
+}
+
+variable "window_size" {
+  type        = string
+}
+
+variable "snow_username" {
+  type        = string
+}
+
+variable "snow_password" {
+  type        = string
+}
+
+variable "snow_url" {
+  type        = string
+}
+
