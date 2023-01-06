bac78fd34c42ef5f2849a59709369c155278c12f
Kumar Sambhav Pandey <sambhavpandey@gmail.com>
2023-01-07 05:14:55 +0530

    Update arm_deploy_memory_alert.tf

--- arm_deploy_memory_alert.tf
+++ arm_deploy_memory_alert.tf
@@ -1,4 +1,50 @@
-resource "azurerm_resource_group_template_deployment" "memory_alert_deploy" {
+
+
+resource "azurerm_monitor_scheduled_query_rules_alert_v2" "example" {
+  name                = "example-msqrv2"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.name.location
+
+  evaluation_frequency = "PT10M"
+  window_duration      = "PT10M"
+  scopes               = [azurerm_log_analytics_solution.vminsights.id]
+  severity             = 4
+  criteria {
+    query                   = <<-QUERY
+      InsightsMetrics\n| where Origin == \"vm.azm.ms\"\n| where Namespace == \"Memory\" and Name == \"AvailableMB\"\n| extend TotalMemory = toreal(todynamic(Tags)[\"vm.azm.ms/memorySizeMB\"]) | extend AvailableMemoryPercentage = (toreal(Val) / TotalMemory) * 100.0\n| summarize AggregatedValue = avg(AvailableMemoryPercentage) by bin(TimeGenerated, 15m), Computer, _ResourceId\n| project Computer, _ResourceId ,AggregatedValue\n
+      QUERY
+    time_aggregation_method = "Maximum"
+    threshold               = 75
+    operator                = "LessThan"
+
+    //resource_id_column    = "client_CountryOrRegion"
+    //metric_measure_column = "CountByCountry"
+    dimension {
+      name     = "client_CountryOrRegion"
+      operator = "Exclude"
+      values   = ["123"]
+    }
+    failing_periods {
+      minimum_failing_periods_to_trigger_alert = 1
+      number_of_evaluation_periods             = 1
+    }
+  }
+
+  auto_mitigation_enabled          = true
+  workspace_alerts_storage_enabled = false
+  description                      = "High Memory Alert - Details in Worknotes"
+  display_name                     = "Memory-alert"
+  enabled                          = true
+  query_time_range_override        = "PT1H"
+  skip_query_validation            = true
+  action {
+    action_groups = [azurerm_monitor_action_group.ag.id]
+  }
+}
+
+
+/*
+  resource "azurerm_resource_group_template_deployment" "memory_alert_deploy" {
   name                 =    "deployment-${data.azurerm_resource_group.rg.name}-memalert"
   resource_group_name  =     data.azurerm_resource_group.rg.name
   deployment_mode      =     "Incremental"
@@ -79,7 +125,7 @@ resource "azurerm_resource_group_template_deployment" "memory_alert_deploy" {
 TEMPLATE
 }
 
-
+*/
 
 
 
