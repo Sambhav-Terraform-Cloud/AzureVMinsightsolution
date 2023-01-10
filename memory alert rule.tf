8369dacc2279c305702429a59740632567a4a069
Kumar Sambhav Pandey <sambhavpandey@gmail.com>
2023-01-10 16:18:41 +1100

    Update memory alert rule.tf

--- memory alert rule.tf
+++ memory alert rule.tf
@@ -40,7 +40,7 @@ resource "azurerm_resource_group_template_deployment" "memAlertDeploy" {
                 "enabled": true,
                 "evaluationFrequency": "PT5M",
                 "scopes": [
-                    "/subscriptions/[parameters("scope")]"
+                    "/subscriptions/[parameters('scope')]"
                 ],
                 "windowSize": "PT5M",
                 "overrideQueryTimeRange": "P2D",
