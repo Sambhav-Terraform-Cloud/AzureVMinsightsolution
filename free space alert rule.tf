a892fb36e30efb7a59309952f6f06e7399cf0b6c
Kumar Sambhav Pandey <sambhavpandey@gmail.com>
2023-01-11 11:55:15 +1100

    Update and rename memory alert rule.tf to free space alert rule.tf

--- free space alert rule.tf
+++ free space alert rule.tf
@@ -1,5 +1,5 @@
-resource "azurerm_resource_group_template_deployment" "memAlertDeploy" {
-  name                 =    "memAlertDeploy"
+resource "azurerm_resource_group_template_deployment" "freespaceAlertDeploy" {
+  name                 =    "freespaceAlertDeploy"
   resource_group_name  =     data.azurerm_resource_group.rg.name
   deployment_mode      =     "Incremental"
   parameters_content = jsonencode({
