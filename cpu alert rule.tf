352599fc9a7f3e735a56e2440f2ec64c81d507e6
Kumar Sambhav Pandey <sambhavpandey@gmail.com>
2023-01-12 14:07:32 +1100

    Update and rename cpualert.tf to cpu alert rule.tf

--- cpu alert rule.tf
+++ cpu alert rule.tf
@@ -2,7 +2,7 @@
 resource "azurerm_monitor_metric_alert" "cpualert" {
   name                = "cpu-alertrule-${data.azurerm_resource_group.rg.name}"
   resource_group_name = data.azurerm_resource_group.rg.name
-  scopes              = data.azurerm_subscription.current.id
+  scopes              = [data.azurerm_subscription.current.id]
   description         = "Higher Thresold"
   target_resource_type = "Microsoft.Compute/virtualMachines"
   target_resource_location = data.azurerm_resource_group.rg.location
