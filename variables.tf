1684ddaf935fd7cd2bcc10ad1d5c776fa8c68517
Kumar Sambhav Pandey <sambhavpandey@gmail.com>
2023-01-13 14:46:35 +1100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,3 @@
-variable "location" {
-  description = "region"
-  type        = string
-}
-
-variable "scope" {
-  type        = string
-}
-
-variable "cpuThresoldPercent" {
-  type        = string
-}
-
-variable "severity" {
-  type        = string
-}
-
-variable "frequency" {
-  type        = string
-}
-
-variable "window_size" {
-  type        = string
-}
-
-
 ############################################################
 ################### Workspace Variables ####################
 ############################################################
